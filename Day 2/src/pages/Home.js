091a1b733be1961dbcdee9ebef32b6f2d4eaad42
Prem-Siva <126150794+Prem-Siva@users.noreply.github.com>
2023-07-12 16:24:41 +0530

    Add files via upload

---
+++ Day 2/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ho.css';
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='h'>
+
+   
+    <div className='flex justify-center items-center text-white text-3xl  h-screen'>
+    <button className='bg-red-800 text-white-100 py-[8px] px-[12px] rounded-[8px] border border-red-700' style={{marginLeft:'60px'}}>
+                   Reserve Now
+                 </button>
+    </div></div>
+  )
+}
+
+export default Home
